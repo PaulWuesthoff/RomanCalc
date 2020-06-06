79bb99819ae0f6ac7a5a6726493337026ae14df9
Paul <paul.wuest>
2020-06-06 13:13:52 +0200

    Initial Commit

--- RomanCalc/ViewController.swift
+++ RomanCalc/ViewController.swift
@@ -1,18 +1,60 @@
-//
-//  ViewController.swift
-//  RomanCalc
-//
-//  Created by Paul Wüsthoff on 06.06.20.
-//  Copyright © 2020 Paul Wüsthoff. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    var numberOnScreen: Double = 0;
+    var previousNumber: Double = 0;
+    var performingMath = false;
+    var operation: Int = 0;
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if(performingMath == true){
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+        }else{
+            if(sender.tag != 15){
+                label.text = ""
+            }
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        }
+        
+    }
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if label.text != "" && sender.tag != 12 && sender.tag != 15{
+            previousNumber =  Double(label.text!)!
+            if sender.tag == 13 {
+                label.text = "-"
+            }
+            else if sender.tag == 14 {
+                label.text = "+"
+            }
+            operation = sender.tag;
+            performingMath = true;
+        }
+        else if sender.tag == 15
+        {
+            if(operation == 13){
+                label.text = String(previousNumber - numberOnScreen)
+            }
+            else if(operation == 14){
+                label.text = String(previousNumber + numberOnScreen)
+            }
+        }else if sender.tag == 12{
+            label.text = "";
+            previousNumber = 0;
+            numberOnScreen = 0;
+            operation = 0;
+        }
+   
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
